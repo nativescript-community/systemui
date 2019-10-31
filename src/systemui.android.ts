47285b1d96bac7ad4bdaee143e4fb25082f68f1a
Martin Guillon <martin.guillon@akylas.fr>
2019-10-31 10:22:00 +0100

    fix: working navbar colors with dialogs on android

--- src/systemui.android.ts
+++ src/systemui.android.ts
@@ -1,10 +1,18 @@
 import * as common from './systemui-common';
 import { Color } from 'tns-core-modules/color';
 import * as app from 'tns-core-modules/application';
-import { device } from 'tns-core-modules/platform';
 import lazy from 'tns-core-modules/utils/lazy';
+import { View } from 'tns-core-modules/ui/core/view';
+import { Frame, topmost } from 'tns-core-modules/ui/frame';
 
 const isPostLollipop = lazy(() => android.os.Build.VERSION.SDK_INT >= 21);
+
+declare module 'tns-core-modules/ui/core/view' {
+    interface View {
+        _getFragmentManager(): androidx.fragment.app.FragmentManager;
+        _dialogFragment: androidx.fragment.app.DialogFragment;
+    }
+}
 
 function getWindow() {
     return app.android.foregroundActivity.getWindow() as android.view.Window;
@@ -31,11 +39,35 @@ export class StatusBar extends common.StatusBar {
             .setSystemUiVisibility(android.view.View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
-
+function findTopView(view: View) {
+    while (view.parent) {
+        view = view.parent as View;
+    }
+    return view;
+}
 export class NavigationBar extends common.NavigationBar {
     updateBarColor(value: Color) {
         if (isPostLollipop) {
-            const window = app.android.foregroundActivity.getWindow() as android.view.Window;
+            let window;
+            const topFrame = topmost();
+            const topView = findTopView(topFrame);
+            if (topView && topView._dialogFragment) {
+                const dialog = topView._dialogFragment.getDialog();
+                if (dialog) {
+                    window = dialog.getWindow();
+                } else {
+                    topView.once('shownModally', () => {
+                        topView._dialogFragment
+                            .getDialog()
+                            .getWindow()
+                            .setNavigationBarColor(value ? value.android : 0);
+                    });
+                    return;
+                }
+            }
+            if (!window) {
+                window = app.android.foregroundActivity.getWindow();
+            }
             window.setNavigationBarColor(value ? value.android : 0);
         }
     }
