6aea9e5da7ae23bb58b47bf5a1670bedb58408a3
Martin Guillon <martin.guillon@akylas.fr>
2019-11-07 15:54:05 +0100

    fix: cleanup

--- src/vue/index.ts
+++
@@ -1,8 +0,0 @@
-const Plugin = {
-    install(Vue) {
-        Vue.registerElement('StatusBar', () => require('../systemui').StatusBar, {});
-        Vue.registerElement('NavigationBar', () => require('../systemui').NavigationBar, {});
-    }
-};
-
-export default Plugin;
