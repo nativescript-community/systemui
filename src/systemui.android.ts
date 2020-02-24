4d93f5ac8977672b719002ffb5af3d90aebc429c
Martin Guillon <martin.guillon@akylas.fr>
2020-02-24 09:26:26 +0100

    fix(android): override to fix non working props in {N}

--- src/systemui.android.ts
+++ src/systemui.android.ts
@@ -1,13 +1,27 @@
-import * as app from 'tns-core-modules/application';
-import { Color } from 'tns-core-modules/color';
-import { View } from 'tns-core-modules/ui/core/view';
-import { topmost } from 'tns-core-modules/ui/frame';
-import lazy from 'tns-core-modules/utils/lazy';
-import * as common from './systemui-common';
+import * as app from "tns-core-modules/application";
+import { Color } from "tns-core-modules/color";
+import { View } from "tns-core-modules/ui/core/view";
+import {
+    topmost,
+    statusBarStyleProperty,
+    androidStatusBarBackgroundProperty,
+    Page,
+} from "tns-core-modules/ui/frame";
+import lazy from "tns-core-modules/utils/lazy";
+import {
+    applyMixins,
+    cssProperty,
+    cssNavigationBarColorProperty,
+    cssStatusBarColorProperty,
+} from "./systemui-common";
 
 const isPostLollipop = lazy(() => android.os.Build.VERSION.SDK_INT >= 21);
 
-declare module 'tns-core-modules/ui/core/view' {
+const SYSTEM_UI_FLAG_LIGHT_STATUS_BAR = 0x00002000;
+const STATUS_BAR_LIGHT_BCKG = -657931;
+const STATUS_BAR_DARK_BCKG = 1711276032;
+
+declare module "tns-core-modules/ui/core/view" {
     interface View {
         _getFragmentManager(): androidx.fragment.app.FragmentManager;
         _dialogFragment: androidx.fragment.app.DialogFragment;
@@ -18,78 +32,89 @@ function getWindow() {
     return app.android.foregroundActivity.getWindow() as android.view.Window;
 }
 
+async function getPageWindow(view: View) {
+    const topView = view.page;
+    if (topView && topView._dialogFragment) {
+        const dialog = topView._dialogFragment.getDialog();
+        if (dialog) {
+            return dialog.getWindow();
+        } else {
+            return new Promise(resolve => {
+                topView.once(View.shownModallyEvent, () => {
+                    resolve(topView._dialogFragment.getDialog().getWindow());
+                });
+            });
+        }
+    }
+    return app.android.foregroundActivity.getWindow();
+}
+
 class PageExtended {
-    @common.cssProperty navigationBarColor: Color;
-    @common.cssProperty statusBarColor: Color;
+    @cssProperty navigationBarColor: Color;
+    @cssProperty statusBarColor: Color;
     // @common.cssProperty statusBarStyle: string;
-    showStatusBar(animated?: boolean) {
-        getWindow()
+    async showStatusBar(animated?: boolean) {
+        const window = await getPageWindow(this as any);
+        window
             .getDecorView()
             .setSystemUiVisibility(android.view.View.SYSTEM_UI_FLAG_VISIBLE);
     }
-    hideStatusBar(animated?: boolean) {
-        getWindow()
+    async hideStatusBar(animated?: boolean) {
+        const window = await getPageWindow(this as any);
+        window
             .getDecorView()
             .setSystemUiVisibility(android.view.View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
-    [common.cssStatusBarColorProperty.setNative](color: Color) {
+    async [cssStatusBarColorProperty.setNative](color: Color) {
         if (isPostLollipop()) {
-            let window;
-            // const topFrame = topmost();
-            // const topView = common.findTopView(topFrame);
-            const topView =(this as any).page;
-            if (topView && topView._dialogFragment) {
-                const dialog = topView._dialogFragment.getDialog();
-                if (dialog) {
-                    window = dialog.getWindow();
-                } else {
-                    topView.once(View.shownModallyEvent, () => {
-                        topView._dialogFragment
-                            .getDialog()
-                            .getWindow()
-                            .setStatusBarColor(color ? color.android : 0);
-                    });
-                    return;
-                }
-            }
-            if (!window) {
-                window = app.android.foregroundActivity.getWindow();
-            }
+            const window = await getPageWindow(this as any);
             window.setStatusBarColor(color ? color.android : 0);
         }
     }
-    [common.cssNavigationBarColorProperty.setNative](color: Color) {
+    async [cssNavigationBarColorProperty.setNative](color: Color) {
         if (isPostLollipop()) {
-            let window;
-            // const topFrame = topmost();
-            const topView =(this as any).page;
-            if (topView && topView._dialogFragment) {
-                const dialog = topView._dialogFragment.getDialog();
-                if (dialog) {
-                    window = dialog.getWindow();
-                } else {
-                    topView.once(View.shownModallyEvent, () => {
-                        topView._dialogFragment
-                            .getDialog()
-                            .getWindow()
-                            .setNavigationBarColor(color ? color.android : 0);
-                    });
-                    return;
-                }
-            }
-            if (!window) {
-                window = app.android.foregroundActivity.getWindow();
-            }
+            const window = await getPageWindow(this as any);
             window.setNavigationBarColor(color ? color.android : 0);
+        }
+    }
+
+    async [statusBarStyleProperty.setNative](
+        value: "dark" | "light" | { color: number; systemUiVisibility: number }
+    ) {
+        if (isPostLollipop()) {
+            const window = await getPageWindow(this as any);
+            const decorView = window.getDecorView();
+
+            if (value === "light") {
+                window.setStatusBarColor(STATUS_BAR_LIGHT_BCKG);
+                decorView.setSystemUiVisibility(
+                    SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                );
+            } else if (value === "dark") {
+                window.setStatusBarColor(STATUS_BAR_DARK_BCKG);
+                decorView.setSystemUiVisibility(0);
+            } else {
+                window.setStatusBarColor(value.color);
+                decorView.setSystemUiVisibility(value.systemUiVisibility);
+            }
+        }
+    }
+    async [androidStatusBarBackgroundProperty.setNative](
+        value: number | Color
+    ) {
+        if (isPostLollipop()) {
+            const window = await getPageWindow(this as any);
+            const color = value instanceof Color ? value.android : value;
+            (<any>window).setStatusBarColor(color);
         }
     }
 }
 
 let mixinInstalled = false;
 export function overridePageBase() {
-    const NSPage = require('tns-core-modules/ui/page').Page;
-    common.applyMixins(NSPage, [PageExtended]);
+    const NSPage = require("tns-core-modules/ui/page").Page;
+    applyMixins(NSPage, [PageExtended]);
 }
 
 export function installMixins() {
