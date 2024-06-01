d25a4241373ff8163363d032cecf6b8be36d677c
farfromrefug <>
2024-06-01 08:58:29 +0200

    chore: project

---
+++ demo-snippets/vue/Basic.vue
@@ -0,0 +1,28 @@
+<template>
+    <Page>
+        <ActionBar title="Sensors Demo" />
+        <GridLayout>
+            
+        </GridLayout>
+    </Page>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { CoreTypes, View, alert, confirm } from '@nativescript/core';
+export default class Basic extends Vue {
+    
+}
+</script>
+
+<style scoped lang="scss">
+ActionBar {
+    background-color: #42b883;
+    color: white;
+}
+Button {
+    background-color: #42b883;
+    color: white;
+}
+</style>
